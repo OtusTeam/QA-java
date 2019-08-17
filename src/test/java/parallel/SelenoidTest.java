3d6b39d0137617b356e4fd6d7ef7564fa301450e
eremin <mitya1705@gmail.com>
2019-08-17 11:54:24 +0300

    add two types of basetest classes

---
+++ src/test/java/parallel/SelenoidTest.java
@@ -0,0 +1,27 @@
+package parallel;
+
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+
+public class SelenoidTest {
+    protected WebDriver driver;
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+        capabilities.setVersion("76.0");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", false);
+
+        driver = new RemoteWebDriver(
+                URI.create("http://localhost:4444/").toURL(),
+                capabilities
+        );
+    }
+}
